0064c8e183855b928d0b75fc61545966180494b8
jaeuklim <jeuk007@skuniv.ac.kr>
2023-11-16 10:33:15 +0900

    step49~56

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -1,16 +1,18 @@
 package com.ll.sb20231114.domain.article.article.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.global.rq.Rq;
 import com.ll.sb20231114.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +21,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
+@Validated
 public class ArticleController {
     private final ArticleService articleService;
     private final Rq rq;
@@ -28,13 +31,18 @@ public class ArticleController {
         return "article/write";
     }
 
+    @Data
+    public static class WriteForm {
+        @NotBlank
+        private String title;
+        @NotBlank
+        private String body;
+    }
+
     @PostMapping("/article/write")
     @ResponseBody
-    RsData write(
-            String title,
-            String body
-    ) {
-        Article article = articleService.write(title, body);
+    RsData write(@Valid WriteForm writeForm) {
+        Article article = articleService.write(writeForm.title, writeForm.body);
 
         RsData<Article> rs = new RsData<>(
                 "S-1",
@@ -49,29 +57,7 @@ public class ArticleController {
         return rs;
     }
 
-    @PostMapping("/article/write2")
-    @SneakyThrows
-    void write2(
-            HttpServletRequest req,
-            HttpServletResponse resp
-    ) {
-        String title = req.getParameter("title");
-        String body = req.getParameter("body");
 
-        Article article = articleService.write(title, body);
-
-        RsData<Article> rs = new RsData<>(
-                "S-1",
-                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
-                article
-        );
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().println(objectMapper.writeValueAsString(rs));
-    }
 
 
     @GetMapping("/article/getLastArticle")
